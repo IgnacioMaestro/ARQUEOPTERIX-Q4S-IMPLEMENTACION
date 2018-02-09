6353d8fe39cf0d3ec163da838d1002e28b9b9fcd
IMaestro <iakimaestro@gmail.com>
2018-02-09 09:51:08 +0100

    Client changes

--- src/Q4S-client-server/Q4SClient/Q4SClientProtocol.cpp
+++ src/Q4S-client-server/Q4SClient/Q4SClientProtocol.cpp
@@ -1,14 +1,16 @@
 #include "Q4SClientProtocol.h"
 
+#include "Q4SClientConfigFile.h"
+
+#include "..\Q4SCommon\ETime.h"
+#include "..\Q4SCommon\Q4SMathUtils.h"
+#include "..\Q4SCommon\EKey.h"
+#include "..\Q4SCommon\Q4SMessage.h"
+#include "..\Q4SCommon\Q4SMessageTools.h"
+#include "..\Q4SCommon\Q4SCalculation.h"
+
 #include <stdio.h>
 #include <sstream>
-
-#include "ETime.h"
-#include "Q4SMathUtils.h"
-#include "Q4SClientConfigFile.h"
-#include "EKey.h"
-#include "Q4SMessage.h"
-#include "Q4SMessageTools.h"
 
 Q4SClientProtocol::Q4SClientProtocol ()
 {
@@ -20,7 +22,7 @@ Q4SClientProtocol::~Q4SClientProtocol ()
     done();
 }
 
-bool Q4SClientProtocol::init()
+bool Q4SClientProtocol::init(unsigned long times, unsigned long milisecondsBetweenTimes)
 {
     // Prevention done call
     done();
@@ -34,10 +36,27 @@ bool Q4SClientProtocol::init()
 
     if (ok)
     {
-        ok &= openConnections();
+        ok &= tryOpenConnectionsTimes(times, milisecondsBetweenTimes);
     }
 
     return ok;
+}
+
+bool Q4SClientProtocol::tryOpenConnectionsTimes(unsigned long times, unsigned long milisecondsBetweenTimes)
+{
+	bool connected = false;
+
+	unsigned long actualTime = 0;
+	while ((actualTime<times) & !connected)
+	{
+		connected = openConnections();
+
+		Sleep(milisecondsBetweenTimes);
+
+		actualTime++;
+	}
+
+	return connected;
 }
 
 void Q4SClientProtocol::done()
@@ -89,34 +108,7 @@ void Q4SClientProtocol::closeConnections()
     }
 }
 
-
-// State managing functions.
-
-bool Q4SClientProtocol::begin()
-{
-    printf("Handshake Phase\n");
-
-    printf("METHOD: begin\n");
-
-    bool ok = true;
-    
-    if( ok )
-    {
-        Q4SMessage message;
-        message.init(Q4SMREQUESTORRESPOND_REQUEST, Q4SMTYPE_BEGIN, "myIP", q4SClientConfigFile.defaultTCPPort);
-        ok &= mClientSocket.sendTcpData( message.getMessageCChar() );
-    }
-
-    if ( ok )
-    {
-        std::string message;
-        mReceivedMessages.readFirst( message );
-    }
-
-    return ok;
-}
-
-bool Q4SClientProtocol::ready()
+bool Q4SClientProtocol::ready(unsigned long stage)
 {
     printf("METHOD: ready\n");
 
@@ -124,9 +116,7 @@ bool Q4SClientProtocol::ready()
     
     if( ok )
     {
-        Q4SMessage message;
-        message.init(Q4SMREQUESTORRESPOND_REQUEST, Q4SMTYPE_READY, "myIP", q4SClientConfigFile.defaultTCPPort);
-        ok &= mClientSocket.sendTcpData( message.getMessageCChar() );
+		ok &= mClientSocket.sendTcpData(Q4SMessage_createReady("myIP", q4SClientConfigFile.defaultTCPPort, stage).c_str());
     }
 
     if ( ok ) 
@@ -136,52 +126,6 @@ bool Q4SClientProtocol::ready()
     }
 
     return ok;
-}
-
-bool Q4SClientProtocol::measure(Q4SMeasurementLimits limits, Q4SMeasurementResult &results)
-{
-    bool measureOk = true;
-
-    printf("MEASURING\n");
-
-    measureOk = Q4SClientProtocol::measureStage0(limits.stage0, results);
-    if (measureOk)
-    {
-        measureOk = Q4SClientProtocol::measureStage1(limits.stage1, results);
-    }
-
-    return measureOk;
-}
-
-void Q4SClientProtocol::continuity(Q4SMeasurementLimits limits)
-{
-    bool stop = false;
-    bool measureOk = true;
-
-    while ( !stop )
-    {
-        Q4SMeasurementResult results;
-        measureOk = measure(limits, results);
-
-        // TODO notificar del error al Server
-        //stop = !measureOk;
-        if (!measureOk)
-        {
-            //Alert
-            std::string alertMessage = "";
-
-            if ( results.latency )
-            {
-                alertMessage += "Latency: " + std::to_string((long double)results.values.latency);
-            }
-            if ( results.jitter )
-            {
-                alertMessage += " Jitter: " + std::to_string((long double)results.values.jitter);
-            }
-
-            //printf(alertMessage.c_str());
-        }
-    }
 }
 
 void Q4SClientProtocol::bwidth()
@@ -195,18 +139,119 @@ void Q4SClientProtocol::cancel()
     mClientSocket.sendTcpData( "CANCEL" );
 }
 
+bool Q4SClientProtocol::handshake(Q4SSDPParams &params)
+{
+    printf("----------Handshake Phase\n");
+
+    printf("METHOD: begin\n");
+
+    bool ok = true;
+    
+    if( ok )
+    {
+		ok &= mClientSocket.sendTcpData(Q4SMessage_createBegin("myIP", q4SClientConfigFile.defaultTCPPort).c_str());
+    }
+
+    if ( ok )
+    {
+        std::string message;
+        ok &= mReceivedMessages.readFirst( message );
+		ok &= Q4SMessageTools_is200OKMessage(message);
+		ok &= Q4SSDP_parse(message, params);
+    }
+
+    return ok;
+}
+
+bool Q4SClientProtocol::negotiation(Q4SSDPParams params, Q4SMeasurementResult &results)
+{
+    printf("----------Negotiation Phase\n");
+
+	bool ok = true;
+
+	ok &= Q4SClientProtocol::ready(0);
+
+    if( ok )
+    {
+		printf("MEASURING\n");
+
+		Q4SMeasurementResult downResults;
+
+		ok = Q4SClientProtocol::measureStage0(params, results, downResults, 20);
+		if (ok)
+		{
+			printf( "END stage0\n");
+			ok &= Q4SClientProtocol::ready(1);
+			if (ok)
+			{
+				ok = Q4SClientProtocol::measureStage1(params, results, downResults);
+			}
+		}
+	}
+
+	if (ok)
+	{
+		mResults = results;
+	}
+
+	return ok;
+}
+
+void Q4SClientProtocol::continuity(Q4SSDPParams params)
+{
+    printf("----------Continuity Phase\n");
+
+	bool stop = false;
+    bool measureOk = true;
+
+    while ( !stop )
+    {
+		printf("MEASURING\n");
+
+        Q4SMeasurementResult results;
+        Q4SMeasurementResult downResults;
+
+		measureOk = Q4SClientProtocol::measureContinuity(params, results, downResults, 20);
+
+        // TODO notificar del error al Server
+        //stop = !measureOk;
+        if (!measureOk)
+        {
+            //Alert
+            std::string alertMessage = "";
+
+            if ( results.latencyAlert )
+            {
+                alertMessage += "Latency: " + std::to_string((long double)results.values.latency);
+            }
+            if ( results.jitterAlert )
+            {
+                alertMessage += " Jitter: " + std::to_string((long double)results.values.jitter);
+            }
+
+            if ( results.packetLossAlert)
+            {
+                alertMessage += " PacketLoss: " + std::to_string((long double)results.values.packetLoss);
+            }
+
+            //printf(alertMessage.c_str());
+        }
+    }
+}
+
 //--private:-------------------------------------------------------------------------------
 
-bool Q4SClientProtocol::measureStage0(Q4SMeasurementStage0Limits limits, Q4SMeasurementResult &results)
+bool Q4SClientProtocol::measureStage0(Q4SSDPParams params, Q4SMeasurementResult &results, Q4SMeasurementResult &downResults, unsigned long pingsToSend)
 {
     bool ok = true;
 
     std::vector<unsigned long> arrSentPingTimestamps;
+	Q4SMeasurementValues downMeasurements;
 
     if (ok)
     {
         // Send regular pings
-        ok &= sendRegularPings(arrSentPingTimestamps);
+		ok &= sendRegularPings(arrSentPingTimestamps, pingsToSend, params.procedure.negotiationTimeBetweenPingsUplink);
     }
     
     if(!ok)
@@ -216,32 +261,165 @@ bool Q4SClientProtocol::measureStage0(Q4SMeasurementStage0Limits limits, Q4SMeas
 
     if (ok)
     {
-        // Wait the established time to start calculation
-        Sleep( (DWORD)q4SClientConfigFile.timeStartCalc);
+		Sleep(q4SClientConfigFile.timeStartCalc);
 
-        // Calculate Latency
-        calculateLatency(mReceivedMessages, arrSentPingTimestamps, results.values.latency, q4SClientConfigFile.showMeasureInfo);
-        printf( "MEASURING RESULT - Latency: %.3f\n", results.values.latency );
+		// Calculate Latency
+        Calculation_calculateLatency(
+			mReceivedMessages, 
+			arrSentPingTimestamps, 
+			results.values.latency, 
+			pingsToSend, 
+			q4SClientConfigFile.showMeasureInfo);
+        printf( "MEASURING RESULT - Latency Up: %.3f\n", results.values.latency );
 
         // Calculate Jitter
-        calculateJitter(mReceivedMessages, results.values.jitter, q4SClientConfigFile.timeBetweenPings, q4SClientConfigFile.showMeasureInfo);
-        printf( "MEASURING RESULT - Jitter: %.3f\n", results.values.jitter );
+		float packetLoss = 0.f;
+        Calculation_calculateJitterStage0(
+			mReceivedMessages, 
+			results.values.jitter,
+			params.procedure.negotiationTimeBetweenPingsUplink, 
+			pingsToSend,
+			q4SClientConfigFile.showMeasureInfo);
+        printf( "MEASURING RESULT - Jitter Up: %.3f\n", results.values.jitter );
+	}
 
-        // Check latency and jitter limits
-        ok &= checkStage0(limits, results);
+    if ( ok ) 
+    {
+		ok &= interchangeMeasurementProcedure(downMeasurements, results);
+		printf( "MEASURING RESULT - Latency Down: %.3f\n", downMeasurements.latency );
+		printf( "MEASURING RESULT - Jitter Down: %.3f\n", downMeasurements.jitter );
     }
     
+	if ( ok ) 
+    {
+		downResults.values = downMeasurements;
+		ok &= Calculation_checkStage0(params.latency, params.jitterUp, params.latency, params.jitterDown, results, downResults);
+    }
+
+	if (!ok)
+	{
+		Calculation_showCheckMessage(results, downResults);
+	}
+
     return ok;
 }
 
-bool Q4SClientProtocol::sendRegularPings(std::vector<unsigned long> &arrSentPingTimestamps)
+bool Q4SClientProtocol::interchangeMeasurementProcedure(Q4SMeasurementValues &downMeasurements, Q4SMeasurementResult results)
+{
+	bool ok = true;
+
+    if ( ok ) 
+    {
+		// Send Info Ping with sequenceNumber 0
+        ok &= mClientSocket.sendTcpData(Q4SMessage_createPing("myIp", q4SClientConfigFile.defaultUDPPort, 0, 0, results.values).c_str());
+    }
+    
+    if (ok)
+    {
+		// Wait to recive the measurements Ping
+        Q4SMessageInfo  messageInfo;
+        ok &= mReceivedMessages.readPingMessage( 0, messageInfo, true );
+		if (ok)
+		{
+			ok &= Q4SMeasurementValues_parse(messageInfo.message, downMeasurements);
+			if (!ok)
+			{
+				printf( "ERROR:Interchange Read measurements fail\n");
+			}
+		}
+		else
+		{
+            printf( "ERROR:Interchange Read PING fail\n");
+            printf( "Messages:\n");
+			std::string toPrint;
+			while (mReceivedMessages.readFirst(toPrint))
+			{
+				printf(toPrint.c_str());
+			}
+		}
+    }
+
+	return ok;
+}
+
+bool Q4SClientProtocol::measureContinuity(Q4SSDPParams params, Q4SMeasurementResult &results, Q4SMeasurementResult &downResults, unsigned long pingsToSend)
 {
     bool ok = true;
 
-    Q4SMessage message;
+    std::vector<unsigned long> arrSentPingTimestamps;
+	Q4SMeasurementValues downMeasurements;
+
+    if (ok)
+    {
+        // Send regular pings
+		ok &= sendRegularPings(arrSentPingTimestamps, pingsToSend, params.procedure.continuityTimeBetweenPingsUplink);
+    }
+    
+    if(!ok)
+    {
+        printf( "ERROR:sendUdpData PING.\n" );
+    }
+
+    if (ok)
+    {
+		Sleep(q4SClientConfigFile.timeStartCalc);
+
+        // Calculate Latency
+        Calculation_calculateLatency(
+			mReceivedMessages, 
+			arrSentPingTimestamps, 
+			results.values.latency, 
+			pingsToSend, 
+			q4SClientConfigFile.showMeasureInfo);
+        printf( "MEASURING RESULT - Latency Up: %.3f\n", results.values.latency );
+
+        // Calculate Jitter
+        Calculation_calculateJitterAndPacketLossContinuity(
+			mReceivedMessages, 
+			results.values.jitter, 
+			params.procedure.continuityTimeBetweenPingsUplink, 
+			pingsToSend,
+			results.values.packetLoss,
+			q4SClientConfigFile.showMeasureInfo);
+        printf( "MEASURING RESULT - Jitter Up: %.3f\n", results.values.jitter );
+        printf( "MEASURING RESULT - PacketLoss Up: %.3f\n", results.values.packetLoss );
+	}
+
+	if (ok)
+	{
+		ok &= interchangeMeasurementProcedure(downMeasurements, results);
+        printf( "MEASURING RESULT - Latency Down: %.3f\n", downMeasurements.latency );
+        printf( "MEASURING RESULT - Jitter Down: %.3f\n", downMeasurements.jitter );
+		printf( "MEASURING RESULT - PacketLoss Down: %.3f\n", downMeasurements.packetLoss );
+	}
+
+	if (ok)
+	{
+        // Check 
+		downResults.values = downMeasurements;
+
+		ok &= Calculation_checkContinuity(
+			params.latency, params.jitterUp, params.packetLossUp, 
+			params.latency, params.jitterDown, params.packetLossDown,
+			results,
+			downResults);
+    }
+    
+	if (!ok)
+	{
+		Calculation_showCheckMessage(results, downResults);
+	}
+
+	return ok;
+}
+
+bool Q4SClientProtocol::sendRegularPings(std::vector<unsigned long> &arrSentPingTimestamps, unsigned long pingsToSend, unsigned long timeBetweenPings)
+{
+    bool ok = true;
+
     unsigned long timeStamp = 0;
     int pingNumber = 0;
-    int pingNumberToSend = 20;
+    int pingNumberToSend = pingsToSend;
 
     for ( pingNumber = 0; pingNumber < pingNumberToSend; pingNumber++ )
     {
@@ -250,22 +428,76 @@ bool Q4SClientProtocol::sendRegularPings(std::vector<unsigned long> &arrSentPing
         arrSentPingTimestamps.push_back( timeStamp );
 
         // Prepare message and send
-        message.init(Q4SMREQUESTORRESPOND_REQUEST, Q4SMTYPE_PING,"myIp", q4SClientConfigFile.defaultUDPPort, pingNumber, timeStamp);
-        ok &= mClientSocket.sendUdpData( message.getMessageCChar() );
+		ok &= mClientSocket.sendUdpData(Q4SMessage_createPing("myIp", q4SClientConfigFile.defaultUDPPort, pingNumber, timeStamp).c_str());
 
         // Wait the established time between pings
-        Sleep( (DWORD)q4SClientConfigFile.timeBetweenPings );
+        Sleep( (DWORD)timeBetweenPings );
     }
 
     return ok;
 }
 
-bool Q4SClientProtocol::measureStage1(Q4SMeasurementStage1Limits limits, Q4SMeasurementResult &results)
+bool Q4SClientProtocol::measureStage1(Q4SSDPParams params, Q4SMeasurementResult &results, Q4SMeasurementResult &downResults)
 {
     bool ok = true;
 
-    // TODO measure bandwith and packet loss
+	Q4SMeasurementValues downMeasurements;
 
+    printf( "Starting:measureStage1.\n" );
+
+    unsigned long initialTimeStamp = ETime_getTime( );
+	unsigned long sequenceNumber = 0;
+	while(ok && (ETime_getTime() < initialTimeStamp + params.procedure.bandwidthTime))
+	{
+		std::string message = Q4SMessage_createBWidth("myIp", q4SClientConfigFile.defaultUDPPort, sequenceNumber);
+		ok &= mClientSocket.sendUdpData( message.c_str() );
+
+		sequenceNumber++;
+	}
+
+	if (ok)
+    {
+		Calculation_calculateBandwidthStage1(sequenceNumber, params.procedure.bandwidthTime, results.values.bandwidth);
+		printf( "MEASURING RESULT - Bandwidth Up: %0.2f\n", results.values.bandwidth );
+	}
+
+	if (ok)
+	{
+		Sleep(q4SClientConfigFile.timeStartCalc);
+
+		// Calculate PacketLoss
+        bool okCalculated = Calculation_calculatePacketLossStage1(mReceivedMessages, results.values.packetLoss);
+		if (!okCalculated)
+		{
+			printf( "PacketLoss Calculation Error");
+		}
+
+        printf( "MEASURING RESULT - PacketLoss Up: %.3f\n", results.values.packetLoss );
+	}
+
+    if(!ok)
+    {
+        printf( "ERROR:sendUdpData BWidth.\n" );
+    }
+
+	if (ok)
+	{
+		ok &= interchangeMeasurementProcedure(downMeasurements, results);
+		printf( "MEASURING RESULT - Bandwidth Down: %.3f\n", downMeasurements.bandwidth);
+		printf( "MEASURING RESULT - PacketLoss Down: %.3f\n", downMeasurements.packetLoss );
+	}
+
+	if (ok)
+	{
+		// Check
+		downResults.values = downMeasurements;
+		ok &= Calculation_checkStage1(params.bandWidthUp, params.packetLossUp, params.bandWidthDown, params.packetLossDown, results, downResults);
+	}
+
+	if (!ok)
+	{
+		Calculation_showCheckMessage(results, downResults);
+	}
 
     return ok;
 }
@@ -332,14 +564,12 @@ bool Q4SClientProtocol::manageUdpReceivedData( )
                 }
 
                 // mandar respuesta del ping
-                char buffer[ 256 ];
                 if (q4SClientConfigFile.showReceivedPingInfo)
                 {
                     printf( "Ping responsed %d\n", pingNumber);
                 }
-                sprintf_s( buffer, "200 OK %d", pingNumber );
-                ok &= mClientSocket.sendUdpData( buffer );
-            
+				ok &= mClientSocket.sendUdpData( Q4SMessage_create200OKBeginResponse(pingNumber).c_str() );
+
                 // encolar el ping y el timestamp para el calculo del jitter
                 mReceivedMessages.addMessage(message, actualTimeStamp);
             }
